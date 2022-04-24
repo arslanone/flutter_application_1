52ef8b56e509af8c43760fa88bc42bc2982f8a81
Arslan <104207215+arslanone@users.noreply.github.com>
2022-04-24 20:42:54 +0500

    Pages

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/home.dart';
+import 'package:flutter_application_1/pages/home.dart';
 
 void main() {
   runApp(MyApp());
