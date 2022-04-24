4a50410b8355ed31b62aed1aa09ee7f77d77e4aa
Arslan <104207215+arslanone@users.noreply.github.com>
2022-04-24 20:21:06 +0500

    commit

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+  int days = 30;
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        theme: ThemeData(
+          primarySwatch: Colors.deepOrange,
+        ),
+        home: MyTextPage());
+  }
+}
+
+class MyTextPage extends StatelessWidget {
+  const MyTextPage({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    // Declaring Variables to use on the app
+    String name = "Patient Buddy App";
+    // ignore: prefer_const_constructors
+    return Scaffold(
+      appBar: AppBar(title: Text(name)),
+      // ignore: prefer_const_constructors
+      body: Center(child: Text("Welcome to Patient Buddy Mobile App")),
+    );
+  }
+}
