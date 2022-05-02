6da6e4603c3626303844bfcd747699ed1018c7ae
Arslan <104207215+arslanone@users.noreply.github.com>
2022-05-02 22:15:21 +0500

    Button added

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -16,6 +16,7 @@
 import 'dart:html';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/pages/home.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({Key? key}) : super(key: key);
@@ -23,45 +24,63 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-      color: Colors.white,
-      child: Column(
-        children: [
-          Image.asset(
-            "assets/images/loginpic.png",
-            fit: BoxFit.cover,
-          ),
-          SizedBox(
-            height: 15.0,
-          ),
-          Text(
-            "Welcome To Patient Buddy!",
-            style: TextStyle(
-              fontSize: 30,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          SizedBox(
-            height: 20.0,
-          ),
-          Padding(
-            padding:
-                const EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),
-            child: Column(
-              children: [
-                TextFormField(
-                  decoration: InputDecoration(
-                      hintText: "Enter Your Good Name:",
-                      labelText: "Username:"),
+        color: Colors.white,
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Image.asset(
+                "assets/images/loginpic.png",
+                height: 270,
+                fit: BoxFit.cover,
+              ),
+              SizedBox(
+                height: 15.0,
+              ),
+              Text(
+                "Welcome To Patient Buddy!",
+                style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
                 ),
-                TextFormField(
-                  decoration: InputDecoration(
-                      hintText: "Enter Your Password:", labelText: "Password:"),
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                    vertical: 16.0, horizontal: 32.0),
+                child: Column(
+                  children: [
+                    TextFormField(
+                      decoration: InputDecoration(
+                          hintText: "Enter Your Good Name:",
+                          labelText: "Username:"),
+                    ),
+                    TextFormField(
+                      obscureText: true,
+                      decoration: InputDecoration(
+                          hintText: "Enter Your Password:",
+                          labelText: "Password:"),
+                    ),
+                    SizedBox(
+                      height: 20.0,
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        // ignore: avoid_print
+                        // print("Welcome To Patient Buddy Mobile App!");
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => HomePage()));
+                      },
+                      child: Text("Get Started"),
+                    )
+                  ],
                 ),
-              ],
-            ),
-          )
-        ],
-      ),
-    );
+              )
+            ],
+          ),
+        ));
   }
 }
