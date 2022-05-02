548cafb6e766e606399cf65470b90fa01d8c524c
Arslan <104207215+arslanone@users.noreply.github.com>
2022-05-02 22:18:47 +0500

    Debugged

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -7,7 +7,7 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         theme: ThemeData(
-          primarySwatch: Colors.lightGreen,
+          primarySwatch: Colors.deepPurple,
         ),
         home: MyTextPage());
   }
